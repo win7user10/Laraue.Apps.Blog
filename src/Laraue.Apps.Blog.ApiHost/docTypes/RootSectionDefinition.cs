ec9a6c3f62e57d44cade31ee875327dbbbc3c553
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2026-04-22 12:08:07 +0300

    add missing RootSectionDefinition.cs

---
+++ src/Laraue.Apps.Blog.ApiHost/docTypes/RootSectionDefinition.cs
@@ -0,0 +1,9 @@
+﻿using Laraue.CmsBackend;
+
+namespace Laraue.Apps.Blog.ApiHost.docTypes;
+
+public class RootSectionDefinition : BaseContentType
+{
+    public string? Icon { get; init; }
+    public string? Description { get; init; }
+}
